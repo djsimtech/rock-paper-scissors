4989237a8a63e3bf4178f6ed400ddee1a2a53408
Danny Simmons <70684895+djsimtech@users.noreply.github.com>
2024-11-20 16:57:40 -0800

    Add files via upload
    Updated Logic

--- script.js
+++ script.js
@@ -1,50 +1,77 @@
-// let randomNumber = Math.floor(Math.random() * 3) + 1;
-// console.log(randomNumber);
-// let choice = '';
-
-// if (randomNumber === 1) {
-//   choice = 'Rock';
-//   console.log(choice);
-// } else if (randomNumber === 2) {
-//   choice = 'Paper';
-//   console.log(choice);
-// } else {
-//   choice = 'Scissors';
-//   console.log(choice);
-// }
-
 let humanScore = 0;
 let computerScore = 0;
 
-function getComputerChoice() {
-  let randomNumber = Math.floor(Math.random() * 3) + 1;
-  console.log('The Random Number for this round is:', randomNumber);
-  let choice = '';
+console.log("The current Human Score is:", humanScore);
+console.log("The current Computer Score is:", computerScore);
 
+// Function to generate a random choice for the computer
+function getComputerChoice() {
+  // Variable to generate a random number between 1 and 3
+  let randomNumber = Math.floor(Math.random() * 3) + 1;
+  console.log("The Random Number for this round is:", randomNumber);
   if (randomNumber === 1) {
-    return 'rock';
+    return "rock";
   } else if (randomNumber === 2) {
-    return 'paper';
+    return "paper";
   } else {
-    return 'scissors';
+    return "scissors";
   }
 }
 
-console.log('The Computer Chose:', getComputerChoice());
+let computerSelection = getComputerChoice();
+console.log("The Computer Chose:", computerSelection);
 
+// Function to Prompt user for input
 function getHumanChoice() {
-  let input = prompt('Choose Rock, Paper, or Scissors');
-  return input.toLocaleLowerCase();
+  // Variable containing valid choices
+  const validChoices = ["rock", "paper", "scissors"];
+  let input = prompt("Choose Rock, Paper, or Scissors");
+
+  // Converts input to lowercase and validates that User provides a valid response without Typo's
+  if (validChoices.includes(input.toLowerCase())) {
+    console.log("Valid Choice!");
+    // If Typo is Found re-run funciton
+  } else {
+    console.log("Invalid Choice.");
+    return getHumanChoice();
+  }
+  // If valid choice, return choice
+  return input.toLowerCase();
 }
 
-console.log('The Human Chose:', getHumanChoice());
+let humanSelection = getHumanChoice();
 
-console.log('Current Human Score:', humanScore);
-console.log('Current Computer Score:', computerScore);
+console.log("The Human Chose:", humanSelection);
 
-function playRound(humanChoice, computerChoice) {}
+function playRound(humanChoice, computerChoice) {
+  if (humanSelection == "rock" && computerSelection == "rock") {
+    return "Rock vs Rock = Tie! Play again";
+  } else if (humanSelection == "rock" && computerSelection == "paper") {
+    computerScore = ++computerScore;
+    return "Computer Wins: Paper beats Rock!";
+  } else if (humanSelection == "rock" && computerSelection == "scissors") {
+    humanScore = ++humanScore;
+    return "Human Wins: Rock beats Scissors!";
+  } else if (humanSelection == "paper" && computerSelection == "paper") {
+    return "paper vs paper = Tie! Play again";
+  } else if (humanSelection == "paper" && computerSelection == "rock") {
+    humanScore = ++humanScore;
+    return "Human Wins: Paper beats Rock!";
+  } else if (humanSelection == "paper" && computerSelection == "scissors") {
+    computerScore = ++computerScore;
+    return "Computer Wins: Scissors beats Paper!";
+  } else if (humanSelection == "scissors" && computerSelection == "scissors") {
+    return "Scissors vs Scissors = Tie! Play again";
+  } else if (humanSelection == "scissors" && computerSelection == "rock") {
+    computerScore = ++computerScore;
+    return "Computer Wins: Rock beats Scissors!";
+  } else if (humanSelection == "scissors" && computerSelection == "paper") {
+    humanScore = ++humanScore;
+    return "Human Wins: Scissors beats Paper!";
+  }
+}
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
+console.log(playRound(humanSelection, computerSelection));
+// console.log(playRound());
+console.log("The current Human Score is:", humanScore);
+console.log("The current Computer Score is:", computerScore);
